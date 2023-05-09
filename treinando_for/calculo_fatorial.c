a5943b3f4fb3297c55b857136876d27eb9305d77
HenriqueFrancaa <henrique23.franca@gmail.com>
2023-05-08 21:56:55 -0300

    O calculo fatorial

---
+++ treinando_for/calculo_fatorial.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+
+	int numero, fatorial;
+
+	scanf("%d", &numero);
+
+	for(fatorial = 1; numero > 1; numero = numero - 1){
+		
+		fatorial = numero * fatorial;
+	}
+
+	printf("%d",fatorial);
+
+	return 0;
+}
+
+
